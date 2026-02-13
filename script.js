8f38cede89535e51b5a1bf353f2fe527e1ce1c50
kiranmoses <kiranmoseserp@gmail.com>
2026-02-14 01:15:19 +0530

    Update message in sayYes function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function sayYes() {
     document.getElementById("result").innerText =
-        "Yayyyyy ❤️ Best decision ever!";
+        "Yayyyyy ❤️ Adhi Atta undaali, Let's go on a date in London!";
 }
 
 function moveButton() {
