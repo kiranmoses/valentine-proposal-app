048d22d87a2c3365ea2c6745879cbd6bef175c68
kiranmoses <kiranmoseserp@gmail.com>
2026-02-14 02:06:48 +0530

    Refactor sayYes function and button positioning
    Refactor the sayYes function to combine strings and adjust random button positioning calculations.

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function sayYes() {
     document.getElementById("result").innerText =
-        "Yayyyyy ❤️, That's my girl !!!
-        Let's go on a date in London!";
+        "Yayyyyy ❤️, That's my girl !!! Let's go on a date in London!";
 }
 
 function moveButton() {
     const btn = document.getElementById("noBtn");
 
-    const x = Math.random() * window.innerWidth * 0.6;
-    const y = Math.random() * window.innerHeight * 0.6;
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 50);
 
     btn.style.position = "absolute";
     btn.style.left = x + "px";
